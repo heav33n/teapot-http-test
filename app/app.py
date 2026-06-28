3c5dbbdc2df4930bbb05e0afb46f9d94244dcc21
heav33n <rvanfx@gmail.com>
2026-06-28 14:54:48 -0400

    Test files

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+app = FastAPI(
+    debug=False,
+    title="TestApp",
+    version="0.0.1",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json",
+    swagger_ui_oauth2_redirect_url="/api/docs/oauth2-redirect",
+)
+
+app.frontend("/", directory="dist")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # In production, replace with your frontend's specific URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/response")
+def return_tea(is_tea: bool = False):
+    if not is_tea:
+        return JSONResponse(
+            status_code=418,
+            content={"response": "I'm not making coffee, sorry."},
+        )
+
+    return {"response": "I'm making tea."}
---
+++ dist/assets/app.js
@@ -0,0 +1,34 @@
+async function getResponse(is_tea) {
+  try {
+    if (is_tea) {
+      var url = "http://127.0.0.1:8000/api/response?is_tea=true";
+      var response = await fetch(url);
+    } else {
+      var url = "http://127.0.0.1:8000/api/response?is_tea=false";
+      var response = await fetch(url);
+    }
+
+    const data = await response.json();
+
+    const response_section = document.getElementById("response_section");
+    const status_code = document.getElementById("status_code");
+    const response_element = document.getElementById("response");
+
+    const raw_resp = document.getElementById("raw_resp");
+    const raw_section = document.getElementById("raw_section");
+
+    raw_section.style.display = "block";
+    response_section.style.display = "flex";
+
+    raw_resp.style.display = "flex";
+
+    status_code.innerText = response.status;
+    status_code.href = url;
+
+    response_element.innerText = data.response;
+
+    raw_resp.innerText = JSON.stringify(data, null, 2);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
